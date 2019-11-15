e45dd4636b4202a705f099ccf3d3b2856a937fc1
todownload <1192755954@qq.com>
2019-11-15 11:07:10 +0800

    master

--- CourseSite/CourseSite/settings.py
+++ CourseSite/CourseSite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '39pdt8@xps#d&aib&enk!@3!hlkgnd$_f17)r-o8eyx6g++8fr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ]
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -116,5 +116,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATIC_URL = '/static/'
