751d75248d03e43d7210488d92f76a56fd8dcb4b
todownload <1192755954@qq.com>
2019-12-10 10:03:59 +0800

    dev

--- CourseSite/Tapp/admin.py
+++ CourseSite/Tapp/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+
+class CourseInline(admin.TabularInline):
+    model=Course
+    extra=1
+
+class TeacherAdmin(admin.ModelAdmin):
+    fields = ['teacher_name','teacher_id','teacher_pwd']
+    inlines=[CourseInline]
+    list_display = ( 'teacher_name', 'teacher_id' )
+
+class StudentAdmin(admin.ModelAdmin):
+    fields = ['stu_name','stu_id','stu_pwd']
+
+class KnowledgeInline(admin.TabularInline):
+    model = Knowledge
+    extra = 1
+
+class AnnouncementInline(admin.TabularInline):
+    model = Announcement
+    extra = 1
+
+class CourseAdmin(admin.ModelAdmin):
+    fieldsets = [ ('Course Info', {'fields':['course_name','teacher','course_id','course_stu_num']}),('DateTime Info', {'fields':['course_starttime','course_endtime']})  ]
+    inlines = [KnowledgeInline, AnnouncementInline]
+
+class AnnouncementAdmin(admin.ModelAdmin):
+    fields = ['course','anno_pubtime','anno_title','anno_content']
+
+
+
+admin.site.register(Teacher,TeacherAdmin)
+admin.site.register(Course,CourseAdmin)
+admin.site.register(Announcement,AnnouncementAdmin)
+admin.site.register(SelectQuestion)
+admin.site.register(DesignQuestion)
+admin.site.register(DrawQuestion)
+admin.site.register(Student,StudentAdmin)
+
+
--- CourseSite/Tapp/views.py
+++ CourseSite/Tapp/views.py
@@ -1,7 +1,82 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse ,Http404,HttpResponseNotAllowed
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+from django.views.decorators.http import require_POST # 装饰器
 
-from django.http import Http404,HttpResponse
+from .models import *
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request,'Tapp/login.html')
+
+def login(request):
+    return render(request,'Tapp/login.html')
+
+def register(request):
+    return render(request,"Tapp/register.html")
+
+@require_POST
+def midRegisterView(request):
+    try:
+        stu_name = request.POST['usr_name']
+        stu_id = request.POST['usr_id']
+        pwd = request.POST['usr_pwd']
+        cof = request.POST['usr_cof']
+        if stu_name =="" or stu_id == "" or pwd != cof:
+            return HttpResponseRedirect(reverse('Tapp:course',args=()))
+        Student.objects.create(stu_name=stu_name,stu_id=stu_id,stu_pwd=pwd)
+        # print("at db")
+        return HttpResponseRedirect(reverse('Tapp:login',args=()))
+    except Exception:
+        return HttpResponseRedirect(reverse('Tapp:register',args=()))
+
+
+@require_POST
+def midLoginView(request): # 处理login的中间层 只能由内部POST访问
+    try:
+        stu = Student.objects.get(stu_id=request.POST['usr_count'])
+        if stu.stu_pwd == request.POST['usr_pwd']:
+            return HttpResponseRedirect(reverse('Tapp:courses',args=()))
+        return HttpResponseRedirect(reverse('Tapp:login',args=()))
+    except Exception:
+        return HttpResponseRedirect(reverse('Tapp:login',args=()))
+
+class CoursesView(generic.ListView):
+    template_name = "Tapp/courses.html"
+    context_object_name = "all_courses"
+    def get_queryset(self):
+        """Return all courses"""
+        return Course.objects.all()
+    # def http_method_not_allowed(self, request):
+    #     return HttpResponseNotAllowed(permitted_methods="POST GET")
+
+# def courses(request): # 课程索引
+#     try:
+#         stu = Student.objects.get(stu_id=request.POST['usr_count'])
+#     except Exception:
+#         # return Http404("Please login first!")
+#         # return HttpResponse("Please login first!") # 避免非用户通过查看源代码访问此页面
+#         return HttpResponseRedirect(reverse('Tapp:login',args=())) # 跳转回会登录页面
+#     else:
+#         all_courses = Course.objects.all()
+#         # outputs = '<br>'.join([c.course_name for c in all_courses])
+#         # return HttpResponse(content=outputs)
+#         context = {"all_courses":all_courses} # 创建上下文对象
+#         return render(request,"Tapp/courses.html",context) # 渲染模板
+
+class CourseDetailView(generic.DetailView): # 接受名为pk的参数 查找相应对象 会返回HTTP404
+    model = Course
+    template_name = "Tapp/course_detail.html"
+    contest_object_name = "course"
+
+# def course_detail(request,c_id): # 依主码来找
+#     c = get_object_or_404(Course,pk=c_id) # 快捷函数 得到对象 或抛出404
+#     context = {"course":c}
+#     return render(request,"Tapp/course_detail.html",context)
+
+
+
+
